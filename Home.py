8ab3c34342853e98e397c27c0011f50d929d4832
DanieleMarobin <daniele.marobin@hotmail.com>
2022-12-18 08:47:10 +0100

    First Commit

---
+++ Home.py
@@ -0,0 +1,17 @@
+# https://share.streamlit.io/
+
+# https://share.streamlit.io/danielemarobin/monitor/main/Home.py
+
+# Bloomberg API installing process
+# https://discuss.streamlit.io/t/installing-packages-from-the-web/27537
+
+# pipreqs --encoding=utf8
+
+import streamlit as st
+
+st.set_page_config(page_title="Trade Flow",layout="wide",initial_sidebar_state="expanded")
+
+st.markdown("# Trade Flow")
+st.markdown("---")
+
+st.sidebar.markdown("# Trade Flow")
