14926a0d04e3a22649b7f085a78bf58b30b96985
DanieleMarobin <daniele.marobin@hotmail.com>
2022-12-18 14:48:05 +0100

    Added AGGrid

--- Home.py
+++ Home.py
@@ -22,7 +22,47 @@ st.set_page_config(page_title="Trade Flow",layout="wide",initial_sidebar_state="
 st.markdown("# Trade Flow")
 st.sidebar.markdown("# Trade Flow")
 
-file_h ='Data/GTIS/Corn_Trade_Flow.csv'
-df_h = gd.read_csv(file_h)
+# input data
+if True:
+    file_h ='Data/GTIS/Corn_Trade_Flow.csv'
+    rows_per_page=20
+    df_h=None
 
-st.dataframe(df_h)
+    if ('df_h' in st.session_state):
+        df_h=st.session_state['df_h']
+    else:
+        st.write('Getting data from Google Drive')
+        st.session_state['df_h'] = gd.read_csv(file_h)
+        df_h=st.session_state['df_h']
+
+
+# Table
+if True:
+    df=df_h
+    if df is not None:    
+        statusPanels = {'statusPanels': [
+            # { 'statusPanel': 'agTotalAndFilteredRowCountComponent', 'align': 'left' },
+            # { 'statusPanel': 'agTotalRowCountComponent', 'align': 'center' },
+            { 'statusPanel': 'agFilteredRowCountComponent', 'align': 'left' },
+            { 'statusPanel': 'agSelectedRowCountComponent', 'align': 'left' },
+            { 'statusPanel': 'agAggregationComponent', 'align': 'left' },
+            ]}
+
+        gb = GridOptionsBuilder.from_dataframe(df)
+        gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=rows_per_page)
+        gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True,rowMultiSelectWithClick=False)
+        gb.configure_selection('multiple', use_checkbox=True)
+        gb.configure_grid_options(enableRangeSelection=True, statusBar=statusPanels)
+        gb.configure_side_bar(defaultToolPanel='test')
+
+        gb.configure_column('trade', headerCheckboxSelection = True, headerCheckboxSelectionFilteredOnly=True)
+        # gb.configure_column('analysis_range', headerCheckboxSelection = True)
+        gridOptions = gb.build()
+
+        grid_response = AgGrid(df, gridOptions=gridOptions, data_return_mode=DataReturnMode.FILTERED, update_mode=GridUpdateMode.MANUAL, columns_auto_size_mode=ColumnsAutoSizeMode.FIT_CONTENTS, enable_enterprise_modules=True)
+        # st.write(grid_response.keys())
+        # st.write('selected_rows')
+        # st.write(grid_response['selected_rows'])
+
+        selected_rows=grid_response['selected_rows']
+        selected_df=pd.DataFrame(grid_response['selected_rows'])
